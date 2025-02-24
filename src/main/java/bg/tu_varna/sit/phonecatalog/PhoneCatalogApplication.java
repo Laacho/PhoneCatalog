640f6f742fce0808d3e930c9e5c1568a57b2bd65
Lachezar Radushev <lachezarradushev@gmail.com>
2025-02-24 14:33:33 +0200

    initial commit

---
+++ src/main/java/bg/tu_varna/sit/phonecatalog/PhoneCatalogApplication.java
@@ -0,0 +1,13 @@
+package bg.tu_varna.sit.phonecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/tu_varna/sit/phonecatalog/PhoneCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.tu_varna.sit.phonecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
